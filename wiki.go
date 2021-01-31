68b267aec92ed833965cffd1b3f0bf113f030d0c
Paul Merrill <pdm@pdm.me>
2021-01-31 12:09:27 -0800

    add Windows build

--- wiki.go
+++ wiki.go
@@ -2,53 +2,29 @@ package main
 
 import (
 	"io/ioutil"
-	"github.com/DataDog/zstd"
 	"log"
 	"net/http"
 )
 
+var store []byte
+
 func viewHandler(w http.ResponseWriter, r *http.Request) {
-	compressed, err := StoreGet()
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-	if compressed == nil {
+	if store == nil {
 		http.NotFound(w, r)
 		return
 	}
 
-	decompressed, err := zstd.Decompress(nil, compressed)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	log.Printf("Decompressing %d -> %d bytes", len(compressed), len(decompressed))
-
-	w.Write(decompressed)
+	w.Write(store)
 }
 
 func saveHandler(w http.ResponseWriter, r *http.Request) {
-	body, err := ioutil.ReadAll(r.Body)
+	data, err := ioutil.ReadAll(r.Body)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	compressed, err := zstd.Compress(nil, body)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
-
-	log.Printf("Compressing %d -> %d bytes", len(body), len(compressed))
-
-	err = StoreSet(compressed)
-	if err != nil {
-		log.Fatal(err)
-		return
-	}
+    store = data
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
@@ -65,13 +41,6 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	log.Println("Starting :)")
-
-	err := StoreConstruct()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer StoreDestroy()
 
 	http.HandleFunc("/", rootHandler)
 
